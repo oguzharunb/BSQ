19315fb1012b38a21bc45dad982bc97c6c37c056
yusudemi <chillywq@gmail.com>
2024-08-31 23:38:10 +0300

    cahnges

--- srcs/map_data.c
+++ srcs/map_data.c
@@ -1,26 +1,5 @@
 #include "../includes/bsq.h"
 
-//this file is for getting data of the file like chars, size, line number
-int	ft_get_l_number(char *file_name)
-{
-	char	buf[80];
-	int		i;
-	int		fd;
-	int		result;
-
-	result = 0;
-	i = 0;
-	fd = open(file_name, O_RDONLY);
-	while (read(fd, buf + i, 1))
-	{
-		if (buf[i] < '0' || buf[i] > '9')
-			break ;
-		result = (result * 10) + (buf[i] - '0');
-		i++;
-	}
-	close(fd);
-	return (result);
-}
 
 char	ft_get_obstacle(char *file_name)
 {
