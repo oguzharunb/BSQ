d48c3fa1f56c894be655fca2a32451c4d94db468
oguzharunb <oguzharunb@gmail.com>
2024-08-31 16:38:37 +0300

    bsq.h modified

--- includes/bsq.h
+++ includes/bsq.h
@@ -2,14 +2,21 @@
 # define BSQ_H
 # include <stdlib.h>
 # include <fcntl.h>
-
-char	ft_get_l_number(char *file_name);
+# include <unistd.h>
+int		ft_strlen(char *str);
+void	ft_putstr(char *str);
+int		ft_size_file(char *file_name);
+int		ft_get_l_number(char *file_name);
+char	ft_get_obstacle(char *file_name);
+char	ft_get_void(char *file_name);
+char	ft_get_full(char *file_name);
 typedef struct s_map_data
 {
-	char	emty;
+	char	empty;
 	char	obst;
-	char	sqrx;
-	int		n_line;
+	char	full;
+	int		file_size;
+	int		line;
 } t_map_data;
 #endif
 
