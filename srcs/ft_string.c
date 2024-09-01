a26efa3b312e70a5e48406b666dfb5566857f05c
oguzharunb <oguzharunb@gmail.com>
2024-09-01 22:41:52 +0300

    ft_is_lowercase

--- srcs/ft_string.c
+++ srcs/ft_string.c
@@ -25,6 +25,22 @@ char	*ft_strdup(char *str)
 	return (newstr);
 }
 
+int	ft_str_is_lowercase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (!(str[i] >= 'a' && str[i] <= 'z'))
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}	
+
 void	ft_putstr(char *str)
 {
 	int	i;
