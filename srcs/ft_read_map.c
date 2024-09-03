d80beaf3fc7155f30e8b299a6baa2b7902fd85bb
yusudemi <chillywq@gmail.com>
2024-09-03 17:48:25 +0300

    readmap

--- srcs/ft_read_map.c
+++ srcs/ft_read_map.c
@@ -16,7 +16,7 @@ int	ft_size_file(char *file_name)
 }
 
 //this file is for getting data of the file like chars, size, line number
-int	ft_get_l_number(char *file_name)//bro
+int	ft_get_l_number(char *file_name)
 {
 	char	buf;
 	int		i;
